dec4df943fa4c2f2c8fb2836bb99f9d995c6fab5
fangxihang <fangxihang@qq.com>
2018-04-16 10:45:06 +0800

    Initial commit

---
+++ login/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import redirect
+from . import models
+from . import forms
+
+
+# Create your views here.
+
+
+
+def index(request):
+    pass
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    if request.session.get('is_login', None):
+        return redirect("/index/")
+    if request.method == "POST":
+        login_form = forms.UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户不存在！"
+        return render(request, 'login/login.html', locals())
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
+
+def register(request):
+    pass
+    return render(request, 'login/register.html')
+
+
+def logout(request):
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/index/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return redirect("/index/")
